2c8c224906e427d29c35455793c93483904449cf
sergiojanko <janko.sergio1998@gmail.com>
2021-09-28 16:39:14 +0200

    groupby-2

--- university.sql
+++ university.sql
@@ -64,5 +64,11 @@ GROUP BY YEAR(`enrolment_date`);
 
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT `office_address`, COUNT(*) AS `n_teachers`
+FROM `teachers`
+GROUP BY `office_address`;
+
+
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
