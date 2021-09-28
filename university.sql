f2075ce04ce205b3ff771fc0dd0597daea3fd7ee
sergiojanko <janko.sergio1998@gmail.com>
2021-09-28 16:46:28 +0200

    groupby-4

--- university.sql
+++ university.sql
@@ -78,3 +78,7 @@ GROUP BY `exam_id`;
 
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT `department_id`, COUNT(*) AS `n_courses`
+FROM `degrees`
+GROUP BY `department_id`;
