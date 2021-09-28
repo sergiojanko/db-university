8ef3a973ce968494b44bc3a4d618ed677973047d
sergiojanko <janko.sergio1998@gmail.com>
2021-09-28 16:35:11 +0200

    groupby-1

--- university.sql
+++ university.sql
@@ -1,3 +1,5 @@
+--  ! SELECT 
+
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
 
 SELECT * 
@@ -46,3 +48,21 @@ FROM `departments`;
 
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+
+SELECT COUNT(*) AS `n_teachers` 
+FROM `teachers`
+WHERE `phone` IS NULL;
+
+
+-- ! GROUP BY
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT COUNT(*) AS `n_enrollments`, YEAR(`enrolment_date`) AS `year`
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
