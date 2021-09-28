bedb92e98f050a53fdeed2c609ef51fb19aae909
sergiojanko <janko.sergio1998@gmail.com>
2021-09-28 16:12:45 +0200

    select-4

--- university.sql
+++ university.sql
@@ -18,6 +18,12 @@ WHERE `date_of_birth` < '1991/09/28';
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 -- laurea (286)
+
+SELECT * 
+FROM `courses`
+WHERE `year` = 1 AND `period` = 'I semestre';
+
+
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 -- 20/06/2020 (21)
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
